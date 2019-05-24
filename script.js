95eedf1be09510f28c2cb59f906c6a74c12351b0
Maxim Konoval <frontendmax@gmail.com>
2019-05-24 17:45:04 +0300

    Finish refactoring

--- script.js
+++ script.js
@@ -1,16 +1,42 @@
-const table = document.querySelector('table')
-const allCells = [... document.querySelectorAll('td')]
-const mainCells = [... document.querySelectorAll('.row-1 td')]
+const table = document.querySelector('.grid-cells')
+const tableCells = [... document.querySelectorAll('.grid-cells > div')]
+const mainCells = [... document.querySelectorAll('.main-row, .main-col')]
+const mainRow = [... mainCells[0].querySelectorAll('div')]
+const mainCol = [... mainCells[1].querySelectorAll('div')]
 
 function handleMouseover(cell) {
   cell.addEventListener('mouseover', () => {
-    mainCells.filter(mainCell => {
-      mainCell.classList.remove('on-hover')
-      if (mainCell.classList.contains(cell.classList)) mainCell.classList.add('on-hover')
+    mainRow.some(mainRowCell => {
+      handleMouseleave(mainRowCell)
+      
+      if (mainRowCell.classList.contains(cell.classList)) {
+        mainRowCell.classList.add('on-hover')
+
+        mainCol.some(mainColCell => {
+          handleMouseleave(mainColCell)
+          mainColCell.classList.contains(`col-${+cell.textContent / +mainRowCell.textContent}`) && mainColCell.classList.add('on-hover')
+        })
+      }
     })
   })
 }
 
-allCells.forEach(handleMouseover)
+function handleMouseleave(cell) {
+  cell.classList.remove('on-hover')
+}
 
-table.addEventListener('mouseleave', () => mainCells.forEach(mainCell => mainCell.classList.remove('on-hover')))
+function handleMainMouseover(mainCell) {
+  mainCell.classList.add('on-hover')
+}
+
+tableCells.forEach(handleMouseover)
+
+mainCells.forEach(mainCell => {
+  mainCell.addEventListener('mouseover', () => handleMainMouseover(mainRow[0]))
+  mainCell.addEventListener('mouseleave', () => handleMouseleave(mainRow[0]))
+})
+
+table.addEventListener('mouseleave', () => {
+  mainRow.forEach(handleMouseleave)
+  mainCol.forEach(handleMouseleave)
+})
